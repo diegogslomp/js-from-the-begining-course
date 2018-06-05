6d28b754a133e5a543b09c7e2fa0f946a4ef229b
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-06-05 15:47:53 -0300

    project folders renamed, added more sections

---
+++ 7.3-easyhttp/app.js
@@ -0,0 +1,55 @@
+ 
+const http = new easyHTTP;
+
+// // GET Posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts){
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// // GET Single Posts
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post){
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Create Data
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom Post'
+};
+
+// Create Post
+http.post('https://jsonplaceholder.typicode.com/posts/', data, function(err, post) {
+  if(err){
+    console.log(err);
+  } else {
+    console.log(post);
+  }
+});
+
+// // Update Post
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post) {
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// // DELETE Posts
+// http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response){
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+
