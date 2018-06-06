b1663872b0677a574d15c89e7a06f7e930a98ab5
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-06-06 10:04:04 -0300

    githubfinder pt5 finished

--- 8.1-githubfinder/github.js
+++ 8.1-githubfinder/github.js
@@ -2,12 +2,14 @@ class Github {
   constructor() {
     this.client_id = 'dcbc4bba7774c5ac5b76';
     this.client_secret = 'a990ff5baa590bf236a59a2c7145f486702f73ca';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc'
   }
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}$client_id=${this.client_id}&client_secret=${this.client_secret}`);
     const profile = await profileResponse.json();
-    return {
-      profile
-    }
+    const repos = await repoResponse.json();
+    return { profile, repos }
   }
 }
--- 8.1-githubfinder/ui.js
+++ 8.1-githubfinder/ui.js
@@ -1,24 +1,91 @@
-// Search input
-const searchUser = document.getElementById('searchUser');
-// Init Github
-const github = new Github;
-
-// Search niput event listener
-searchUser.addEventListener('keyup', e =>{
-  // Get input text
-  const userText = e.target.value;
-  if(userText !== '') {
-    // Make http call
-    github.getUser(userText).then(data => {
-      if(data.profile.message === 'Not Found') {
-        // Show alert
-        alert("Not Found")
-      } else {
-        // Show profile
-        console.log(data)
-      }
-    });
-  } else {
-    // Clear profile
+class UI {
+  constructor() {
+    this.profile = document.getElementById('profile');
   }
-})
+
+  showProfile(user) {
+    // Clear alerts if exists
+    this.clearAlert();
+    this.profile.innerHTML = `
+      <div class="card card-body mb-3">
+        <div class="row">
+          <div class="col-md-3">
+            <img class="img-fluid mb-2 center" src="${user.avatar_url}"/>
+            <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+          </div>
+          <div class="col-md-9">
+            <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
+            <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
+            <span class="badge badge-success">Followers: ${user.followers}</span>
+            <span class="badge badge-info">Following: ${user.following}</span>
+            <br><br>
+            <ul class="list-group">
+              <li class="list-group-item">Company: ${user.company}</li>
+              <li class="list-group-item">Website/Blog: ${user.blog}</li>
+              <li class="list-group-item">Location: ${user.location}</li>
+              <li class="list-group-item">Member Since: ${user.created_at}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <h3 class="page-heading mb-3">Latest Repos</h3>
+      <div id="repos"></div>
+    `;
+    // console.log(user);
+  }
+
+  // Show repos
+  showRepos(repos) {
+    let output = '';
+    repos.forEach(repo => {
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </div>
+            <div class="col-md-6">
+              <span class="badge badge-primary">Starts: ${repo.stargazers_count}</span>
+              <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+              <span class="badge badge-success">Forks: ${repo.forks_count}</span>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+    // Output repos
+    document.getElementById('repos').innerHTML = output;
+  }
+
+  // Show alert
+  showAlert(message, className) {
+    // Clear current alert if exists
+    this.clearAlert();
+    // Create div alert
+    const div = document.createElement('div');
+    div.className = className;
+    div.appendChild(document.createTextNode(message));
+    // Get parent and insert before position
+    const container = document.querySelector('.searchContainer');
+    const search = document.querySelector('.search');
+    // Insert alert
+    container.insertBefore(div, search);
+    // Timeout after 3 sec
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  // Clear alert message
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+    if (currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+  // Clear showed profile
+  clearProfile() {
+   this.profile.innerHTML = ''; 
+  }
+}
