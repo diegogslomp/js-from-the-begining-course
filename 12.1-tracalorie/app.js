f44577f3f5fd40d513eae77c533edf49e061d126
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-06-10 23:04:03 -0300

    sec12lec102

--- 12.1-tracalorie/app.js
+++ 12.1-tracalorie/app.js
@@ -6,7 +6,24 @@ const ItemCtrl = ((id, name, calories) => {
     this.name = name;
     this.calories = calories;
   }
-  console.log("ItemCTRLemmm");
+
+  // Data Structure / State
+  const data = {
+    items: [
+      { id: 0, name: 'Steak Dinner', calories: 1200},
+      { id: 1, name: 'Cookie', calories: 400},
+      { id: 2, name: 'Eggs', calories: 300}
+    ],
+    currentItem: null,
+    totalCalories: 0
+  }
+
+  // Public methods
+  return {
+    logData: () => {
+      return data;
+    }
+  }
 
 })();
 
@@ -20,5 +37,13 @@ const StorageCtrl = (() => {
 
 // App Controller
 const App = ((ItemCtrl, UICtrl, StorageCtrl) => {
+  // Public methods
+  return {
+    init: () => {
+      console.log('Initializing App...');
+    }
+  }
 })(ItemCtrl, UICtrl, StorageCtrl);
+
+App.init();
 
