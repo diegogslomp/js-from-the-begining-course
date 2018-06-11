1cc68452802fa1b1ea77437556df05afebb2f182
Diego Gobbi Slomp <diegogslomp@gmail.com>
2018-06-11 10:36:30 -0300

    sec12lec104

--- 12.1-tracalorie/app.js
+++ 12.1-tracalorie/app.js
@@ -1,7 +1,7 @@
 // Item Controller
 const ItemCtrl = ((id, name, calories) => {
   // Item Constructor
-  const Item = () => {
+  const Item = function(id, name ,calories) {
     this.id = id;
     this.name = name;
     this.calories = calories;
@@ -18,8 +18,32 @@ const ItemCtrl = ((id, name, calories) => {
     totalCalories: 0
   }
 
-  // Public methods
   return {
+    // Public methods
+    getItems: () => {
+      return data.items;
+    },
+
+    addItem: (name, calories) => {
+      let ID;
+      // Create id
+      if(data.items.length > 0){
+        ID = data.items[data.items.length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+      // Calories to number
+      calories = parseInt(calories);
+
+      // Create new item
+      newItem = new Item(ID, name, calories);
+
+      // Add new item to array
+      data.items.push(newItem);
+
+      return newItem;
+    },
+
     logData: () => {
       return data;
     }
@@ -29,6 +53,34 @@ const ItemCtrl = ((id, name, calories) => {
 
 // UI Controller
 const UICtrl = (() => {
+  const UISelectors = {
+    itemList: '#item-list',
+    itemNameInput: '#item-name',
+    itemCaloriesInput: '#item-calories',
+    addBtn: '.add-btn'
+  }
+  // Public methods
+  return {
+    populateItemLists: (items) => {
+      html = '';
+      items.forEach(item => {
+        html += `<li class="collection-item" id="item-${item.id}">
+                  <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                  <a href="#" class="secondary-content"><i class="edit-item fa fa-pencil-alt"></i></a>
+                 </li>`
+      });
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    },
+    getSelectors: () => {
+      return UISelectors;
+    },
+    getItemInput: () => {
+      return {
+        name: document.querySelector(UISelectors.itemNameInput).value,
+        calories: document.querySelector(UISelectors.itemCaloriesInput).value
+      };
+    }
+  }
 })();
 
 // Storage Controller
@@ -37,10 +89,36 @@ const StorageCtrl = (() => {
 
 // App Controller
 const App = ((ItemCtrl, UICtrl, StorageCtrl) => {
+  // Load event listeners
+  const loadEventListeners = () => {
+    // Get UI selectors
+    const UISelectors = UICtrl.getSelectors();
+    // Add item event
+    document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+  }
+  // Add event submit
+  const itemAddSubmit = (e) => {
+    // Get form input from UI Controller
+    const input = UICtrl.getItemInput();
+
+    // Check for name and calorie input
+    if(input.name !== '' && input.calories !== '') {
+      // Add item
+      const newItem = ItemCtrl.addItem(input.name, input.calories);
+    } 
+    e.preventDefault();
+  }
   // Public methods
   return {
     init: () => {
-      console.log('Initializing App...');
+      // Fetch items from data structure
+      const items = ItemCtrl.getItems();
+
+      // Populate list with items
+      UICtrl.populateItemLists(items);
+
+      // Load event listeners
+      loadEventListeners();
     }
   }
 })(ItemCtrl, UICtrl, StorageCtrl);
